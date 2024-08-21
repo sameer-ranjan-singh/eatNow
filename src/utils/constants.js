094ea92725e85ac486b32e97c5f0521e961b0c13
Sameer Ranjan Singh <sameer.lancer99@gmail.com>
2024-08-21 12:57:09 +0530

    configuring const file url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,9 @@
+import { BASE_URL } from "../../config"
+
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
-export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.51800&lng=88.38320&restaurantId="
-export const SWIGGY_API ="https://www.swiggy.com/dapi/restaurants/list/v5/?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+export const MENU_API = BASE_URL + "api/swiggy/menu"
+export const SWIGGY_API = BASE_URL + "api/swiggy/restaurants"
+
+// export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
+// export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.51800&lng=88.38320&restaurantId="
+// export const SWIGGY_API ="https://www.swiggy.com/dapi/restaurants/list/v5/?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
